9f45d8b81cd8cb74067ac77fd9ecd8a016418960
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2024-12-11 00:14:28 +0500

    Make a userName scene to get scene and transferred main game ui to gameScene method

--- src/main/java/com/example/semesterproject/BattleShip/BattleShipUI.java
+++ src/main/java/com/example/semesterproject/BattleShip/BattleShipUI.java
@@ -8,113 +8,56 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
 public class BattleShipUI extends Application {
     public void start(Stage primaryStage) {
-        BattleShip backend = new BattleShip();
-        BorderPane uiLayout = new BorderPane();
-        uiLayout.setStyle("-fx-background-color:  black;-fx-padding: 10px,3px,3px,3px");
-        uiLayout.setPrefWidth(1100);
-        uiLayout.setPrefHeight(1100);
-        //labels for stats
+        BorderPane userNameLayout = new BorderPane();
+        userNameLayout.setStyle("-fx-background-color:  black;-fx-padding: 10px,3px,3px,3px");
+        primaryStage.setWidth(1100);
+        primaryStage.setHeight(1050);
+        Image image = new Image("battleship.jpg");
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(500);
+        imageView.setFitWidth(1100);
+        userNameLayout.setTop(imageView);
+        Label nameLabel = new Label("Username");
+        nameLabel.setStyle("-fx-text-fill: white;-fx-font-weight: bold;-fx-font-size: 18px;");
+        HBox nameHBox = new HBox();
+        nameHBox.setPrefWidth(150);
+        nameHBox.setAlignment(Pos.CENTER);
 
+        TextField username = new TextField();
+        nameHBox.getChildren().add(username);
+        Label userNameresult = new Label();
+        userNameresult.setStyle("-fx-text-fill: white;-fx-font-weight: bold;-fx-font-size: 18px;");
+        username.setPromptText("Username");
+        Button submitButton = new Button("Submit");
+        submitButton.setStyle("-fx-background-color:  Green ; -fx-text-fill: white;-fx-font-weight: bold; -fx-font-size: 18px;");
+        submitButton.setOnAction(e -> {
+            if(username.getText().equals("")) {
+               userNameresult.setText("Username cannot be empty");
+               return;
 
-        ArrayList<Button> coordinatesOfShips = new ArrayList<>();
-        GridPane gridOfShip = new GridPane();
-        gridOfShip.setAlignment(Pos.CENTER);
-        uiLayout.setCenter(gridOfShip);
-        this.initializeShipGrid(coordinatesOfShips, gridOfShip,backend);
-        //setting up the remaining ships,hits and misses updates at bottom left
-        Label remainingShips = this.getStyledlabel("RemainingShips:"+backend.remainingShips);
-        Label hits = this.getStyledlabel("Hits:"+backend.hits);
-        Label misses = this.getStyledlabel("Misses:"+backend.misses);
-        VBox fieldsBox = new VBox();
-
-        fieldsBox.getChildren().addAll(remainingShips, hits, misses);
-        fieldsBox.setAlignment(Pos.BOTTOM_LEFT);
-        fieldsBox.setSpacing(10);
-
-
-        Button resetButton = new Button("Reset");
-        resetButton.setAlignment(Pos.BOTTOM_CENTER);
-        resetButton.setOnAction(e -> {
-            this.resetGame(backend,coordinatesOfShips,gridOfShip,remainingShips,hits,misses);
-        });
-        //bottom layout
-        HBox bottomLayout = new HBox();
-        bottomLayout.getChildren().addAll(fieldsBox, resetButton);
-        bottomLayout.setPrefWidth(1000);
-        bottomLayout.setSpacing(645);
-
-        resetButton.setStyle("-fx-background-color: #f44336; -fx-text-fill: white; -fx-font-size: 18px;");
-        resetButton.setPrefWidth(160);
-        uiLayout.setBottom(bottomLayout);
-        //top layout
-        HBox topLayout = new HBox();
-        Label username =this.getStyledlabel("Username:");
-        Button hintButton = new Button("Hint");
-        hintButton.setStyle("-fx-background-color:  Green ; -fx-text-fill: white; -fx-font-size: 18px;");
-        hintButton.setPrefWidth(160);
-        topLayout.setPrefWidth(1000);
-        topLayout.setSpacing(750);
-        hintButton.setOnAction(actionEvent -> {
-            int [] shipPosition = backend.hintSystem();
-            if(shipPosition!=null) {
-                for (Button currentButton : coordinatesOfShips) {
-                    if (GridPane.getRowIndex(currentButton) == shipPosition[0] && GridPane.getColumnIndex(currentButton) == shipPosition[1]) {
-                        currentButton.setStyle("-fx-padding: 2px;-fx-font-size: 30px;-fx-font-weight: bold;-fx-text-fill: white;-fx-background-color:yellow;-fx-border-width: 2px;-fx-border-style: solid;-fx-border-color: white;-fx-border-radius: 2px");
-                    }
-                }
             }
+            this.gameScene(primaryStage,username.getText());
         });
+        VBox inputBox = new VBox();
+        inputBox.setSpacing(15);
+        inputBox.setStyle("-fx-padding: 10px");
+        inputBox.setAlignment(Pos.TOP_CENTER);
+        inputBox.getChildren().addAll(nameLabel, nameHBox, submitButton,userNameresult);
+        userNameLayout.setCenter(inputBox);
 
-        hintButton.setAlignment(Pos.TOP_CENTER);
-        topLayout.getChildren().addAll(username,hintButton);
-        uiLayout.setTop(topLayout);
-
-        for(Button currentShipCoordinate : coordinatesOfShips){
-            currentShipCoordinate.setOnAction(event -> {
-                int row = GridPane.getRowIndex(currentShipCoordinate);
-                int col = GridPane.getColumnIndex(currentShipCoordinate);
-                try {
-                    if(currentShipCoordinate.getText().equalsIgnoreCase("HIT")||currentShipCoordinate.getText().equalsIgnoreCase("MISS")){
-                        return;
-                    }
-                   else  if(backend.processGuess(row,col)){
-                        currentShipCoordinate.setStyle("-fx-padding: 2px;-fx-font-size: 20px;-fx-font-weight: bold;-fx-text-fill: white;-fx-background-color:green;-fx-border-width: 2px;-fx-border-style: solid;-fx-border-color: white;-fx-border-radius: 2px");
-                        currentShipCoordinate.setText("HIT");
-
-                    }
-                    else{
-                        currentShipCoordinate.setStyle("-fx-padding: 2px;-fx-font-size: 20px;-fx-font-weight: bold;-fx-text-fill: white;-fx-background-color:red;-fx-border-width: 2px;-fx-border-style: solid;-fx-border-color: white;-fx-border-radius: 2px");
-                        currentShipCoordinate.setText("MISS");
-                    }
-
-                    this.updatedStats(remainingShips,hits,misses,backend);
-                if(backend.isGameOver()) {
-                    Alert gameResult = new Alert(Alert.AlertType.INFORMATION);
-                    gameResult.setTitle("Game Over");
-                    gameResult.setHeaderText("Victory");
-                    gameResult.setContentText("Congratulations!You Won");
-                    gameResult.showAndWait();
-
-                }
-                } catch (RowColumnException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        }
-        Scene battleShipScene = new Scene(uiLayout);
         primaryStage.setTitle("Battle Ship");
         primaryStage.setResizable(false);
-        primaryStage.setScene(battleShipScene);
+        primaryStage.setScene(new Scene(userNameLayout));
         primaryStage.show();
     }
     public static void main(String[] args) {
@@ -164,4 +107,100 @@ public class BattleShipUI extends Application {
         backend.restartGame();
         this.updatedStats(remainingShips,hits,Misses,backend);
     }
+    public void gameScene(Stage primaryStage ,String userName) {
+        BattleShip backend = new BattleShip();
+        BorderPane uiLayout = new BorderPane();
+        uiLayout.setStyle("-fx-background-color:  black;-fx-padding: 10px,3px,3px,3px");
+
+
+        //labels for stats
+
+
+        ArrayList<Button> coordinatesOfShips = new ArrayList<>();
+        GridPane gridOfShip = new GridPane();
+        gridOfShip.setAlignment(Pos.CENTER);
+        uiLayout.setCenter(gridOfShip);
+        this.initializeShipGrid(coordinatesOfShips, gridOfShip,backend);
+        //setting up the remaining ships,hits and misses updates at bottom left
+        Label remainingShips = this.getStyledlabel("RemainingShips: "+backend.remainingShips);
+        Label hits = this.getStyledlabel("Hits: "+backend.hits);
+        Label misses = this.getStyledlabel("Misses: "+backend.misses);
+        VBox fieldsBox = new VBox();
+
+        fieldsBox.getChildren().addAll(remainingShips, hits, misses);
+        fieldsBox.setAlignment(Pos.BOTTOM_LEFT);
+        fieldsBox.setSpacing(10);
+
+
+        Button resetButton = new Button("Reset");
+        resetButton.setAlignment(Pos.BOTTOM_CENTER);
+        resetButton.setOnAction(e -> {
+            this.resetGame(backend,coordinatesOfShips,gridOfShip,remainingShips,hits,misses);
+        });
+        //bottom layout
+        HBox bottomLayout = new HBox();
+        bottomLayout.getChildren().addAll(fieldsBox, resetButton);
+        bottomLayout.setPrefWidth(1000);
+        bottomLayout.setSpacing(640);
+
+        resetButton.setStyle("-fx-background-color: #f44336; -fx-text-fill: white; -fx-font-size: 18px;-fx-font-weight: bold");
+        resetButton.setPrefWidth(160);
+        uiLayout.setBottom(bottomLayout);
+        //top layout
+        HBox topLayout = new HBox();
+        Label username =this.getStyledlabel("Username: "+userName);
+        Button hintButton = new Button("Hint");
+        hintButton.setStyle("-fx-background-color:  Green ;-fx-font-weight: bold; -fx-text-fill: white; -fx-font-size: 18px;");
+        hintButton.setPrefWidth(160);
+        topLayout.setPrefWidth(1100);
+        topLayout.setSpacing(640);
+        hintButton.setOnAction(actionEvent -> {
+            int [] shipPosition = backend.hintSystem();
+            if(shipPosition!=null) {
+                for (Button currentButton : coordinatesOfShips) {
+                    if (GridPane.getRowIndex(currentButton) == shipPosition[0] && GridPane.getColumnIndex(currentButton) == shipPosition[1]) {
+                        currentButton.setStyle("-fx-padding: 2px;-fx-font-size: 30px;-fx-font-weight: bold;-fx-text-fill: white;-fx-background-color:yellow;-fx-border-width: 2px;-fx-border-style: solid;-fx-border-color: white;-fx-border-radius: 2px");
+                    }
+                }
+            }
+        });
+
+        hintButton.setAlignment(Pos.TOP_CENTER);
+        topLayout.getChildren().addAll(username,hintButton);
+        uiLayout.setTop(topLayout);
+
+        for(Button currentShipCoordinate : coordinatesOfShips){
+            currentShipCoordinate.setOnAction(event -> {
+                int row = GridPane.getRowIndex(currentShipCoordinate);
+                int col = GridPane.getColumnIndex(currentShipCoordinate);
+                try {
+                    if(currentShipCoordinate.getText().equalsIgnoreCase("HIT")||currentShipCoordinate.getText().equalsIgnoreCase("MISS")){
+                        return;
+                    }
+                    else  if(backend.processGuess(row,col)){
+                        currentShipCoordinate.setStyle("-fx-padding: 2px;-fx-font-size: 20px;-fx-font-weight: bold;-fx-text-fill: white;-fx-background-color:green;-fx-border-width: 2px;-fx-border-style: solid;-fx-border-color: white;-fx-border-radius: 2px");
+                        currentShipCoordinate.setText("HIT");
+
+                    }
+                    else{
+                        currentShipCoordinate.setStyle("-fx-padding: 2px;-fx-font-size: 20px;-fx-font-weight: bold;-fx-text-fill: white;-fx-background-color:red;-fx-border-width: 2px;-fx-border-style: solid;-fx-border-color: white;-fx-border-radius: 2px");
+                        currentShipCoordinate.setText("MISS");
+                    }
+
+                    this.updatedStats(remainingShips,hits,misses,backend);
+                    if(backend.isGameOver()) {
+                        primaryStage.close();
+
+
+                    }
+                } catch (RowColumnException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        Scene battleShipScene = new Scene(uiLayout);
+        primaryStage.setScene(battleShipScene);
+    }
+
 }
+
